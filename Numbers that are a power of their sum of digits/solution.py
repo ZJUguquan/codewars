bf9e5ad21b290b5d9fff458e3efeca46c4be2a77
Stan <szakratsky@gmail.com>
2016-09-14 15:48:26 +0400

    Add files via upload

---
+++ Numbers that are a power of their sum of digits/solution.py
@@ -0,0 +1,29 @@
+'''
+Best practice:
+
+series = [0]
+for a in range(2, 99):
+    for b in range(2, 42):
+        c = a**b
+        if a == sum(map(int, str(c))):
+            series.append(c)
+power_sumDigTerm = sorted(series).__getitem__
+'''
+
+def DigitSum(n):
+    nString = str(n)
+    result = 0
+    for digit in nString:
+        result += int(digit)
+    return result
+
+def power_sumDigTerm(n):
+    resultTable = list()
+    for i in range(2, 100):
+        value = i
+        for j in range(2, 20):
+            value *= i
+            if(DigitSum(value) == i):
+                resultTable.append(value)
+    resultTable.sort()
+    return resultTable[n-1] # n-th term of the sequence, each term is a power of the sum of its digits
